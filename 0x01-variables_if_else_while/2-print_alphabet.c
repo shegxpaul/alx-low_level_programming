e8bb50fdbdcf2d41e0332ada321593d8f56f649c
shegxpaul <shegxpail@gmail.com>
2022-10-16 12:00:14 -0700

    12

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
