22d72ce24235543b674b6d6dd36a1b9b3870ee41
shegxpaul <shegxpail@gmail.com>
2022-10-18 12:02:44 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main"
+/**
+ * add - add two munbers
+ * @a: the firdt number
+ * @b: the secondnumber
+ * Return: the addition of both
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
