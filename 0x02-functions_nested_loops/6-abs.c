e43d6cb296606b3b4f9236965262bccebb675ec5
shegxpaul <shegxpail@gmail.com>
2022-10-18 11:03:31 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value of an integer
+ * @t: th einteger to be computed
+ * Return: the sbsolute value
+ */
+
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
+
