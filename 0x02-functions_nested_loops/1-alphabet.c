40b1045b5bfcf57c93a7f7a69c8c925b350bd40a
shegxpaul <shegxpail@gmail.com>
2022-10-18 09:57:01 -0700

    more _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in LC
+ *
+ */
+
+void print_alphabet(void)
+{
+	char UP;
+
+	for (UP = 'a'; UP <= 'z'; UP++)
+		_putchar(UP);
+
+	_putchar('\n');
+}
