1c529171689eb1c2b723927e5894858d92faf488
shegxpaul <shegxpail@gmail.com>
2022-10-20 08:54:45 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "mian.h"
+
+/**
+ * print _most_numbers - print the numbers 0 -9
+ * without 2 and 4
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int h = 0;
+
+	while (h < 10)
+	{
+		if (h != 2 && h != 4)
+			_putchar(h + '0');
+		h++;
+	}
+	_putchar('\n');
+}
