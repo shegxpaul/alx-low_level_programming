c9f76020c4f2ddb644ecc8943b221644aa132dc4
shegxpaul <shegxpail@gmail.com>
2022-10-20 10:03:13 -0700

    10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_traingle - prints a traingle
+ * @n: size of the triang;le
+ */
+
+void print_triangle(int n)
+{
+	int h, tri;
+
+	if (n > 0)
+	{
+		for (h = 1; h <= n h++)
+		{
+			for (tri = n - h; tri > 0; tri--)
+				_putchar(' ');
+			for (tri = 0; tri < h; tri++)
+				_putchar('0');
+			if (h == n)
+				continue;
+			_putchar('\n');
+		}
+	}
+	+putchar('\n');
+}
