e2068e94ca171c168c1ae96ea3a5ec330e6b8b7e
shegxpaul <shegxpail@gmail.com>
2022-10-20 08:38:14 -0700

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integars
+ * @a: first int
+ * @b: secnd int
+ * Return: the multiplication of and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
