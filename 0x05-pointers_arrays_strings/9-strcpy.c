02af7e0128b2dead661c4cfb78a47846bbd999a8
shegxpaul <shegxpail@gmail.com>
2022-10-24 13:15:54 -0700

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "msin.h"
+
+/**
+ * _strcpy - copies the string pointed to by scr
+ * @dest: destimnation
+ * @src: source
+ * return: string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+
+	int len = 0;
+
+	while (*(src * len) != '\0')
+	{
+		*(dest + len) = *(src + len);
+		len++;
+	}
+
+	*(dest + len) = '\0';
+	return (dest);
+}
