a8eab06e9c954db9b6dce5eac28846808e44674a
shegxpaul <shegxpail@gmail.com>
2022-10-24 12:06:12 -0700

    t2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integars
+ * @a: int a
+ * @b: int b
+ */
+
+void swap_int(int *a, int *b)
+{
+	int q;
+
+	q = *a;
+	*a = *b;
+	*b = q;
+}
