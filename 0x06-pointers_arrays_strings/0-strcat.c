7cd647a06a98eadfbd0c81118f09a7a58ee20f47
shegxpaul <shegxpail@gmail.com>
2022-10-26 14:33:50 -0700

    ts0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - concaternates two str
+ * @dest: copy to
+ * @src: copy from
+ * Return: destination
+ */
+
+
+char *_strcat(char *dest, char *src);
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '0\')
+	{
+		desr[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
