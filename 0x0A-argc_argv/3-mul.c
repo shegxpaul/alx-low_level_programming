ef21e5481f9eeb3239686df64308106df0973066
shegxpaul <shegxpail@gmail.com>
2022-11-06 14:05:28 -0800

    ts3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - prints the multiplication of tewo integars
+ * @argc: arguments count
+ * @argv: arguments vector
+ * Return: 0 if true, 1 when false
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc == 3)
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%d\n", a * b);
+		return (0);
+	}
+	printf("Error\n");
+	return (1);
+}
